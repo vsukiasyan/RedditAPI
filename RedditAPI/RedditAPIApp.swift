27673594aaaf33935b13de418bdedf3ef111196a
vsukiasyan <vsukiasyan1@hotmail.com>
2021-07-01 14:40:45 -0700

    Initial Commit

---
+++ RedditAPI/RedditAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditAPIApp.swift
+//  RedditAPI
+//
+//  Created by Vic Sukiasyan on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RedditAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
